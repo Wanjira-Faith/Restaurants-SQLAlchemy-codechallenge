a1e71f17371ec2050db29da31ee0212145b21f42
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-01 12:36:12 +0300

    imported necessary modules from SQLAlchemy

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,Column,Integer,String,ForeignKey,ForeignKey
 from sqlalcemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,relationship
 
-engine = create_engine('sqlite///:restaurants.db')
+
+engine = create_engine('sqlite///:restaurants.db', echo=True)
 
 Base = declarative_base()
