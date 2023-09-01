b570afb2bc1a57b51afa751340496eab640e0060
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-01 13:10:59 +0300

    added the data models

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,25 @@ engine = create_engine('sqlite:///restaurant.db', echo=True)
 #base class for all the classes
 Base = declarative_base()
 
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)  
+    name = Column(String)  
+    price = Column(Integer)  
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+class Review(Base):
+    __tablename__ ='reviews'
+
+    id = Column(Integer, primary_key=True)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    star_rating = Column(Integer)
