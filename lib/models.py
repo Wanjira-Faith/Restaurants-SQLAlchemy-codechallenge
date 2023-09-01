494dd7149625822f4aef6967e029a173a3648696
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-01 13:09:51 +0300

    created empty migration

--- lib/models.py
+++ lib/models.py
@@ -1,32 +1,10 @@
 from sqlalchemy import create_engine,Column,Integer,String,ForeignKey,ForeignKey
-from sqlalcemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 
 # Define database commnection
-engine = create_engine('sqlite///:restaurant.db', echo=True)
+engine = create_engine('sqlite:///restaurant.db', echo=True)
 
 #base class for all the classes
 Base = declarative_base()
 
-
-class Restaurant(Base):
-    __tablename__ = 'restaurants'
-
-    id = Column(Integer, primary_key=True)  
-    name = Column(String)  
-    price = Column(Integer)  
-
-class Customer(Base):
-    __tablename__ = 'customers'
-
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-
-class Review(Base):
-    __tablename__ ='reviews'
-
-    id = Column(Integer, primary_key=True)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    customer_id = Column(Integer, ForeignKey('customers.id'))
-    star_rating = Column(Integer)
