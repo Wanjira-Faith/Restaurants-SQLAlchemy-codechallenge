21a2941facfb8583ab4598ab764e753c68b5ce7c
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-01 16:26:50 +0300

    added data to session and commit

--- lib/seed.py
+++ lib/seed.py
@@ -21,7 +21,8 @@ review2 = Review(restaurant=restaurant1, customer=customer2, star_rating=5)
 review3 = Review(restaurant=restaurant2, customer=customer1, star_rating=7)
 review4 = Review(restaurant=restaurant3, customer=customer3, star_rating=8)
 
-
-
+# Add data to session and commit
+session.add_all([restaurant1, restaurant2, restaurant3, customer1, customer2, customer3, review1, review2, review3, review4])
+session.commit()
 
 
