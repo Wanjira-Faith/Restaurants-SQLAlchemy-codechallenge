21a6bd94c26911acc293b1f22ba9bdc954941bc8
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-01 16:00:16 +0300

    created engine and session

--- lib/seed.py
+++ lib/seed.py
@@ -2,6 +2,10 @@ from  models import Restaurant, Customer, Review
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('sqlite:///restaurant.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 
