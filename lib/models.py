ed922dae41497117266f78d6e2c3daac74cbe8b4
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-01 12:37:08 +0300

    added comments

--- lib/models.py
+++ lib/models.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine,Column,Integer,String,ForeignKey,ForeignKey
 from sqlalcemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 
-
+# Define database commnection
 engine = create_engine('sqlite///:restaurants.db', echo=True)
 
+#base class for all the classes
 Base = declarative_base()
